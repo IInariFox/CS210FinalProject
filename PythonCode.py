d3ae0afc1f11780b3b98a06ae26302a495424b39
nathon <nathonchavez01@gamil.com>
2022-10-10 12:54:39 -0500

    Improved code so has less warnings

--- PythonCode.py
+++ PythonCode.py
@@ -1,57 +1,129 @@
 import re
 import string
+import os.path
+from os import path
+
+
+#Description:
+#To call this function, pass the function name into the CallProcedure function.
+#Example:
+#callProcedure("CountAll");
+#Output:
+#Returns a list of all unique items in CS210, and prints all of their occurrances.
+#Return:
+#       None
+def CountAll():
+    #Open the file in read mode
+    text = open("CS210_Project_Three_Input_File.txt", "r")
+
+    #Create an empty dictionary to store "found" words
+    dictionary = dict()
+
+    #Check each line of the input file
+    for line in text:
+        #remove any errant spaces and newline characters
+        line = line.strip()
+
+        #Convert characters to lowercase for better matching
+        word = line.lower()
+
+        #Check if the word is already in the dictionary
+        if word in dictionary:
+            #Increment number of times the word appears
+            dictionary[word] = dictionary[word] + 1
+        else:
+            #If the word is not in the dictionary, add it w/ a value of 1
+            dictionary[word] = 1
+
+    #Print all contents of the dictionary
+    for key in list (dictionary.keys()):
+        print(key.capitalize(), ":", dictionary[key])
+
+    #Close the opened file.
+    text.close()
 
 
 
-def getFreqAllItems(string = ""):  #this method returns a string which contains all the items & their respective frequency
-    filename = "input.txt" #give your input file name here, it should be in the same directory where python code is present
-    f= open(filename, "r") #open input data file
-    if not f:
-        print("Could not open the file, please check the file name") #print error message if , file is not opened
-    else:
-        data= f.read() #read file read
-        listItems= data.split() #split the data into individual items & store them in a list
-        frequency = {} # dictionary to store item:frequeny
-        for item in listItems:
-            frequency[item]= frequency.get(item,0)+1 #calculating frequency
+#Description:
+#       To call this function, pass this function into the CallIntFunc function along with the desired search term.
+#Example:
+#       callIntFunc("CountInstances", searchTerm);
+#Output:
+#       Returns the number of occurrances of the desired search term.
+#Return:
+#       int wordCount
+def CountInstances(searchTerm):
 
-        return_string = "" #answer string
-        for item,freq in frequency.items():
-            return_string+= item + " : " + str(freq) + '\n' #appending data to answer string
-        return return_string  #return answer_string
+    #Convert user-inputted search term to lowercase for better matching
+    searchTerm = searchTerm.lower()
+
+    #Open the file in read mode
+    text = open("CS210_Project_Three_Input_File.txt", "r")
+
+    #Create variable to track how many times the search term has been "found"
+    wordCount = 0
+
+    #Check each line of the input file
+    for line in text:
+        #remove any errant spaces and newline characters
+        line = line.strip()
+
+        #Convert characters to lowercase for better matching
+        word = line.lower()
+        #Check if the found word is equal to the user's input
+        if word == searchTerm:
+            #Increment number of times the word appears
+            wordCount += 1
+
+    #Return the number of times the search term was found, as per specification
+    return wordCount
+
+    #Close opened file
+    text.close()
 
 
 
-def getFrequencySingleItem(item_name):#this method returns a string which contains the given item & respective frequency
-    filename = "input.txt"  #give your input file name here, it should be in the same directory where python code is present
-    f= open(filename, "r")  #open input data file
-    if not f:
-        print("Could not open the file, please check the file name") #print error message if , file is not opened
-    else:
-        data= f.read()  #read file read
-        listItems= data.split()   #split the data into individual items & store them in a list
-        frequency = {}  # dictionary to store item:frequeny
-        for item in listItems:
-            frequency[item]= frequency.get(item,0)+1   #calculating frequency
+#Count the number of appearances for each item, then write to frequency.dat
 
-        if item_name in frequency:  #if given item is present in the string
-            return frequency[item_name]  #return the frequency
-        return "NO such item present"  #else return no such item present
+#Description:
+#       To call this function, pass this function into the CallProcedure function.
+#Example:
+#       callProcedure("CollectData");
+#Output:
+#       Returns a a document (frequency.dat) with each unique word and the number of times that word occurs..
+#Return:
+#       frequency.dat
+def CollectData():
+    #Open the input file in read mode
+    text = open("CS210_Project_Three_Input_File.txt", "r")
 
-def showHistogram(string=""):
-    filename = "input.txt"  #give your input file name here, it should be in the same directory where python code is present
-    f= open(filename, "r")  #open input data file
-    if not f:
-        print("Could not open the file, please check the file name") #print error message if , file is not opened
-    else:
-        data= f.read()  #read file read
-        listItems= data.split()   #split the data into individual items & store them in a list
-        frequency = {}  # dictionary to store item:frequeny
-        for item in listItems:
-            frequency[item]= frequency.get(item,0)+1   #calculating frequency
+    #Create and/or write the file frequency.dat
+    frequency = open("frequency.dat", "w")
 
-        ans=""  #answer string
-        for item,freq in frequency.items():
-            ans+=item + " " +  '*'* freq + '\n'  #appending item, '*' no.of times the item occures
+    #Create an empty dictionary to store "found" words
+    dictionary = dict()
 
-        return ans #returning answer
+    #Check each line of the input file
+    for line in text:
+        #remove any errant spaces and newline characters
+        line = line.strip()
+
+        #Convert characters to lowercase for better matching
+        word = line.lower()
+
+        #Check if the word is already in the dictionary
+        if word in dictionary:
+            #Increment number of times the word appears
+            dictionary[word] = dictionary[word] + 1
+        else:
+            #If the word is not in the dictionary, add it w/ a value of 1
+            dictionary[word] = 1
+
+    #Write each key and value pair to frequency.dat
+    for key in list (dictionary.keys()):
+        #Format the key-value pair as strings followed by a newline.
+        frequency.write(str(key.capitalize()) + " " + str(dictionary[key]) + "\n")
+
+    #Close the opened files
+    text.close()
+    frequency.close()
